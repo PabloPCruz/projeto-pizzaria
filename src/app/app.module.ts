36a2935a25951eb17d4449a87c99a026ec74b4af
PabloPCruz <infoexpresscwb@gmail.com>
2025-07-29 21:00:40 -0300

    style: estilização utilizando angular/animtations entre páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ExtraOptions } from '@angular/router';
 
 import { App } from './app'; // seu componente raiz
 import { Header } from './header/header';
@@ -11,10 +11,23 @@ import { Contacts } from './contacts/contacts';
 import { Menu } from './menu/menu';
 import { routes } from './app.routes';
 import { Template } from './template/template';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routerOptions: ExtraOptions = {
+  anchorScrolling: 'enabled',
+  scrollPositionRestoration: 'enabled',
+};
 
 @NgModule({
   declarations: [App, Header, Presentation, Contacts, Menu, Template],
-  imports: [BrowserModule, FlavorsModule, MatCardModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    FlavorsModule,
+    MatCardModule,
+    RouterModule.forRoot(routes, routerOptions),
+    BrowserAnimationsModule,
+    
+  ],
   bootstrap: [App]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,17 @@
-
 import { Routes } from '@angular/router';
 
 // components
-
 import { Menu } from './menu/menu';
-import { Contacts } from './contacts/contacts';
-import { Presentation } from './presentation/presentation';
 import { Template } from './template/template';
-import { Header } from './header/header';
-import { Flavors } from './flavors/flavors';
+
 
 export const routes: Routes = [
     {
         path: '',
-        component: Template,
-        children: [
-            { path: 'header', component: Header },
-            { path: 'presentation', component: Presentation },
-            { path: 'best-flavors', component: Flavors },
-            { path: 'contacts', component: Contacts },
-        ]
+        component: Template, // página única com todas as seções
     },
-    { path: 'menu', component: Menu },
+    {
+        path: 'menu',
+        component: Menu
+    }
 ];
